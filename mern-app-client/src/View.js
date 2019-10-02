dd7bf9920127abc2913e59cc6fcde63898cb49b2
PeteXC <peterchai2008@hotmail.co.uk>
2019-10-02 16:30:51 +0100

    build1

--- mern-app-client/src/View.js
+++ mern-app-client/src/View.js
@@ -4,7 +4,7 @@ export default class View extends React.Component {
   render() {
     return (
       <div>
-        <h1> HELLO WELCOME TO VIEW </h1>
+        <h1> HELLO WELCOME TO VIEW! </h1>
       </div>
     );
   }
